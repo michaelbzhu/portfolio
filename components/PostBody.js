eb21591ad055ed6bcfd8c639bbe479e461f4c1ad
Michael Zhu <mbzhu1@gmail.com>
2021-11-13 13:24:47 -0800

    add twitter script to head

--- components/PostBody.js
+++ components/PostBody.js
@@ -1,10 +1,20 @@
 import markdownStyles from "./post-styles.module.css";
+import Head from "next/head";
 
 export default function PostBody({ content }) {
   return (
-    <div
-      className={markdownStyles["post"]}
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <>
+      <Head>
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+      </Head>
+      <div
+        className={markdownStyles["post"]}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </>
   );
 }
