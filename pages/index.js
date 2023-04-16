e59cf4acb6136e814fe2bfa9e6bcd48dac2473cf
Michael Zhu <mbz@Michaels-MacBook-Pro.local>
2023-04-15 22:28:55 -0700

    update content

--- pages/index.js
+++ pages/index.js
@@ -56,126 +56,21 @@ export default function Home() {
           Hi, I'm Michael
         </h2>
         <p className="text-left text-lg leading-7 my-3">
-          Third year CS student at
+          Currently a software engineer at
           <ExternalLink
+            href="https://cs.berkeley.edu/"
+            className="underline text-black decoration-slate-500 decoration-2"
+          >
+            Retool
+          </ExternalLink>
+        </p>
+        <p className="text-left text-lg leading-7 my-3">
+          Previously studied computer science at <ExternalLink
             href="https://cs.berkeley.edu/"
             className="underline text-black decoration-blue-500 decoration-2"
           >
             UC Berkeley
-          </ExternalLink>
-        </p>
-        <p className="text-left text-lg leading-7 my-3 ">
-          Head of Engineering at
-          <ExternalLink
-            href="https://dormroomfund.com/"
-            className="underline text-black decoration-purple-500 decoration-2"
-          >
-            Dorm Room Fund
-          </ExternalLink>
-          , a student run venture fund that invests in the best student founders
-        </p>
-        {/* <p className="text-left text-lg leading-7 my-3 ">
-          Occasionally jot down some random
-          <ExternalLink
-            href="https://thoughts.michaelbzhu.com"
-            className="underline text-black decoration-cyan-500 decoration-2"
-          >
-            thoughts
-          </ExternalLink>
-        </p> */}
-        <h2 className="text-left text-2xl font-bold leading-7 mt-10">
-          I'm currently interested in
-        </h2>
-        <p className="text-left text-lg leading-7 my-3">
-          How to be a{" "}
-          <ExternalLink
-            href="https://www.julian.com/blog/vanity-metrics"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            better
-          </ExternalLink>{" "}
-          <ExternalLink
-            href="https://www.kalzumeus.com/2011/10/28/dont-call-yourself-a-programmer"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            software
-          </ExternalLink>{" "}
-          <ExternalLink
-            href="https://thezbook.com/"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            engineer
-          </ExternalLink>
-        </p>
-
-        <p className="text-left text-lg leading-7 my-3">
-          <ExternalLink
-            href="https://open.spotify.com/playlist/0jCdplasbHs0OZMEu1VOUI?si=20009ff1d9dc4814"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            Chinese
-          </ExternalLink>
-          ,
-          <ExternalLink
-            href="https://noahpinion.substack.com/"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            economics
-          </ExternalLink>
-          , and
-          <ExternalLink
-            href="https://danwang.co/"
-            className="text-black underline decoration-pink-500 decoration-2"
-          >
-            chinese economics
-          </ExternalLink>
-        </p>
-
-        <h2 className="text-left text-2xl font-bold leading-7 mt-10">
-          Selected Projects
-        </h2>
-        <p className="text-left text-lg leading-7 my-3 ">
-          <ExternalLink
-            href="https://ugly.video"
-            className="text-black underline decoration-emerald-500 decoration-2"
-            trail
-          >
-            ugly.video
-          </ExternalLink>
-          - implemented browser based video conferencing using WebRTC
-        </p>
-        <p className="text-left text-lg leading-7 my-3 ">
-          <ExternalLink
-            href="https://michael-docs.s3.us-west-1.amazonaws.com/robust_image_classifier.pdf"
-            className="text-black underline decoration-emerald-500 decoration-2"
-            trail
-          >
-            Improving Free Adversarial Training
-          </ExternalLink>{" "}
-          - researched methods of reducing neural network vulnerablity to FGSM
-          attacks
-        </p>
-        <p className="text-left text-lg leading-7 my-3 ">
-          <ExternalLink
-            href="https://medium.com/ludwig-ai/hyperparameter-optimization-with-ludwig-6e31272e43fb"
-            className="text-black underline decoration-emerald-500 decoration-2"
-            trail
-          >
-            Sentiment Analysis with Ludwig
-          </ExternalLink>
-          - comparative analysis of CNN vs LSTM vs BERT performance using
-          Ludwig's deep learning toolkit
-        </p>
-        <p className="text-left text-lg leading-7 my-3 ">
-          <ExternalLink
-            href="https://supply-website.vercel.app/"
-            className="text-black underline decoration-emerald-500 decoration-2"
-            trail
-          >
-            Supply
-          </ExternalLink>
-          - another failed pandemic social app. Created an iOS app that enables
-          community sharing of essential items
+          </ExternalLink>.
         </p>
         <h2 className="text-left text-2xl font-bold leading-7 mt-10">Links</h2>
         <p className="text-left text-lg leading-7 my-3 ">
